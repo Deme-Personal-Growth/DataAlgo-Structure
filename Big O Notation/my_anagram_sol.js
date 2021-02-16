23c89ebeee13572fef61d5440e85b7125ffa3cb2
metrio <dlima.nyit@gmail.com>
2021-02-16 14:45:38 -0500

    Reunderstood the problem

--- Big O Notation/my_anagram_sol.js
+++ Big O Notation/my_anagram_sol.js
@@ -1,5 +1,6 @@
 //Questions to ask, do we care if the letter is uppercase or not?
 //Are we also check punctuation?
+//This is for actual 
 
 
 isAnagram = (str1, str2) => {
@@ -7,13 +8,23 @@ isAnagram = (str1, str2) => {
         return false
     } 
 
-    const newStr1 = str1.split("").reverse()
-    const newStr2 = str2.split("")
+    const str1Obj = {}
+    const str2Obj = {}
 
-    console.log(newStr1, newStr2)
+    for(let char of str1){
+        str1Obj[char] = (str1Obj[char] || 0) + 1
+    }
 
-    for(let val in newStr1){
-        if(newStr1[val] !== newStr2[val]){
+    for(let char of str2){
+        str2Obj[char] = (str2Obj[char] || 0) + 1
+    }
+
+    console.log(str1Obj, str2Obj)
+
+    for(let key in str1Obj){
+        if(!(key in str2Obj)){
+            return false
+        } else if(str2Obj[key] !== str1Obj[key]) {
             return false
         }
     }
@@ -22,6 +33,6 @@ isAnagram = (str1, str2) => {
 }
 
 console.log(isAnagram("", ""))
-console.log(isAnagram("Hello", "olleH"))
+console.log(isAnagram("Hello", "olHle"))
 console.log(isAnagram("gogono", "ogogon"))
 console.log(isAnagram("1", "2"))
