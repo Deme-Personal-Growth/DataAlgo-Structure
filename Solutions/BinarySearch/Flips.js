b8f1c8a0ba26577f06adcf9134ed719bde57c87a
metrio <dlima.nyit@gmail.com>
2021-04-09 11:02:47 -0400

    oh wow

--- Solutions/BinarySearch/Flips.js
+++ Solutions/BinarySearch/Flips.js
@@ -1,3 +1,12 @@
+// You are given an integer list nums containing 0s and 1s. 
+// Consider an operation where we pick an index i in nums and flip i as 
+// well as all numbers to the right of i. 
+// Return the minimum number of operations required to make nums contain all 0s.
+
+// Constraints
+
+// n ≤ 100,000 where n is the length of nums.
+
 class Solution {
     solve(nums) {
         let neededFlips = 0
