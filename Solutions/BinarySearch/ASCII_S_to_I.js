22dce4890e2b794c53bdc14b09e1939b81b1bb8e
metrio <dlima.nyit@gmail.com>
2021-04-15 11:16:31 -0400

    String to Int

---
+++ Solutions/BinarySearch/ASCII_S_to_I.js
@@ -0,0 +1,19 @@
+/** You are given a string s contianing digits from
+ * "0" to "9" and lowercase alphabet characters. Return 
+ * the sum of the numbers found in s
+ */
+
+ class Solution {
+    solve(s) {
+       const justNums = s.split(/[^0-9]/)
+
+       return justNums.reduce((sum, value) => {
+           if(value !== ''){
+                return sum += parseInt(value)
+            } else {
+                return sum
+            } 
+       }, 0)
+
+    }
+}
