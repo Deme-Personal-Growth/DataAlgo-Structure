10a066558cedf20cdc043286487d92b776b5fa52
metrio <dlima.nyit@gmail.com>
2021-04-28 22:40:52 -0400

    Finally solved maxSubArr(greedy)

--- Solutions/Section6 - Optional Challenges/maxSubArrSum.js
+++ Solutions/Section6 - Optional Challenges/maxSubArrSum.js
@@ -2,28 +2,29 @@ function maxSubarraySum(arr, n){
     // add whatever parameters you deem necessary - good luck!
     
     let maxSum = Number.NEGATIVE_INFINITY
-    let currentSum
     let i = 0
     let ii = 1
+    let currentSum = arr[i] + arr[ii]
+    const limit = n - 1
     
     while (ii <= arr.length - 1 ){
         
-        
-        if( (ii - i ) === (n - 1) ){
-           currentSum = arr[i] + arr[ii]
-           console.log(maxSum, currentSum)
-           maxSum = Math.max(maxSum, currentSum)
-           i++
-           ii = i + 1
-        } else {
-        
-          if( (ii - i) > (n - 1) ){
+        if( (ii - i ) === limit ){
+          maxSum = Math.max(maxSum, currentSum)
+          i++
+          ii = i + 1
+          currentSum = arr[i] + arr[ii]
+       }
+          if( (ii - i) < limit ){
+              ii++
+              currentSum += arr[ii]
+          }
+
+          if( (ii - i) > limit){
+              currentSum -= arr[i]
               i++
           }
-          if( (ii - i) < (n - 1) ){
-              ii++
-          }
-        }
+
     }
     
     if(maxSum === Number.NEGATIVE_INFINITY) return null
@@ -31,3 +32,5 @@ function maxSubarraySum(arr, n){
     return maxSum
    
   }
+
+  console.log(maxSubarraySum([1, 4, 2 ,10, 23, 3, 1, 0, 20], 4))
