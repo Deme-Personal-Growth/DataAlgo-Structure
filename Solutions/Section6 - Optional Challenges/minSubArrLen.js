644cf05e216879b2d2a4aeb769b6907bd4643a9b
metrio <dlima.nyit@gmail.com>
2021-04-28 22:45:22 -0400

    Start minSubArrLen

---
+++ Solutions/Section6 - Optional Challenges/minSubArrLen.js
@@ -0,0 +1,14 @@
+/**
+ * Writer a function called minSubArrLen which accepts two parameters - an array of positiver
+ * integers and a positive integer
+ * 
+ * @param {*} arr 
+ * @param {*} n 
+ * 
+ * This function should return the minimal length of a cotinguous subarray of which the sum
+ * is greater than or equal to the integer passed to the function. If there isn't one, return
+ * 0
+ */
+function minSubArrLen(arr, n){
+
+}
