9d9637b15aaf5b26cc6da8e3d2516758cd0555e2
metrio <dlima.nyit@gmail.com>
2021-06-06 22:22:22 -0400

    First attempt at CountZero

--- Solutions/InfoFrom Skilled/totalNumZeros.js
+++ Solutions/InfoFrom Skilled/totalNumZeros.js
@@ -1,3 +1,25 @@
 // 4- Count Total number of zeros from 1 upto n
 // 	countZero(2014) = 223
 // 	countZero(100) = 11
+
+const totalZeros = (n) => {
+    let count = 0
+    const objN = {}
+    
+    if (n === 1) return 0
+    if (objN[n]) return objN[n]
+    
+    if(n % 10 === 0){
+        count++
+        objN[n] = count
+        totalZeros(n / 10)
+    }
+
+    if(n % 10 !== 0 ){
+        objN[n] = count
+        totalZeros(n - 1)
+    }
+
+}
+
+console.log(totalZeros(2014))
